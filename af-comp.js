2d53b76e2099770516ed574e566843718da52221
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2024-01-09 12:48:33 -0500

    reintroduce video control

--- af-comp.js
+++ af-comp.js
@@ -3,3 +3,23 @@
 function off() {
     document.getElementById("overlay").style.display = "none";
 };
+
+//video control
+AFRAME.registerComponent('vidhandler', {
+    init: function () {
+      this.toggle = false;
+      this.vid = document.querySelector("#ld-vid")
+      this.vid.pause();
+  },
+  tick:function(){
+     if(this.nftmarkerFound){
+       if(!this.toggle){
+         this.toggle = true;
+         this.vid.play();
+      }
+    }else if(this.nftmarkerLost){
+      this.toggle = false;
+      this.vid.pause();
+    }
+   }
+  });

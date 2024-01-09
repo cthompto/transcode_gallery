42ddbb01764203acce48cce6088efad2cd0716c0
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2024-01-09 11:10:43 -0500

    fix error in components script

--- af-comp.js
+++ af-comp.js
@@ -8,9 +8,9 @@ document.addEventListener('DOMContentLoaded', function() {
 var assets = document.querySelector('a-scene');
 var loading = document.querySelector('#loading');
 
-assets.addEventListener('loaded', function (e) {
-    loading.style.display = 'none';
-});
+// assets.addEventListener('loaded', function (e) {
+//     loading.style.display = 'none';
+// });
 
 });
 

f8fe639c91b20b8805df056d7bb4dd5130b14017
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2024-01-09 13:28:58 -0500

    video delay

--- af-comp.js
+++ af-comp.js
@@ -1,25 +1,11 @@
 // overlay control
 
 function off() {
-    document.getElementById("overlay").style.display = "none";
-};
+  document.getElementById("overlay").style.display = "none";
+  var vid = document.querySelector("#ld-vid");
 
-//video control
-AFRAME.registerComponent('vidhandler', {
-    init: function () {
-      this.toggle = false;
-      this.vid = document.querySelector("#ld-vid")
-      this.vid.pause();
-  },
-  tick:function(){
-     if(this.nftmarkerFound){
-       if(!this.toggle){
-         this.toggle = true;
-         this.vid.play();
-      }
-    }else if(this.nftmarkerLost){
-      this.toggle = false;
-      this.vid.pause();
-    }
-   }
-  });
+  var delayInMilliseconds = 2500; //1 second
+  setTimeout(function () {
+    vid.play();
+  }, delayInMilliseconds);
+}

6e10a8dfeb3b33154c32ec4f9c914c5b54eb165a
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2024-01-09 12:27:06 -0500

    removing unused code

--- af-comp.js
+++ af-comp.js
@@ -3,34 +3,3 @@
 function off() {
     document.getElementById("overlay").style.display = "none";
 };
-// loading function
-document.addEventListener('DOMContentLoaded', function() {
-var assets = document.querySelector('a-scene');
-var loading = document.querySelector('#loading');
-
-// assets.addEventListener('loaded', function (e) {
-//     loading.style.display = 'none';
-// });
-
-});
-
-// video control
-
-AFRAME.registerComponent('vidhandler', {
-    init: function () {
-      this.toggle = false;
-      this.vid = document.querySelector("#ld-vid")
-      this.vid.pause();
-  },
-  tick:function(){
-     if(this.nftmarkerFound){
-       if(!this.toggle){
-         this.toggle = true;
-         this.vid.play();
-      }
-    }else if(this.nftmarkerLost){
-      this.toggle = false;
-      this.vid.pause();
-    }
-   }
-  });
